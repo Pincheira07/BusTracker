be9449daa5e1b6ac7885486b1439ffce133ada21
camilo09m <c.nanco01@ufromail.cl>
2021-12-20 13:06:24 -0300

    Commit final 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
+import com.guis.MicrosLlinea;
+import com.guis.SignIn;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         //Inicio inicio = new Inicio();
         //inicio.ejecutarMenu();
-
-        MinisterioTransporte ministerioTransporte = new MinisterioTransporte();
-        System.out.print(ministerioTransporte.getLineas().toString());
-
-
-
+        MicrosLlinea ml = new MicrosLlinea();
+       // MinisterioTransporte ministerioTransporte = new MinisterioTransporte();
+        //System.out.print(ministerioTransporte.getLineas().toString());
+}
 }
