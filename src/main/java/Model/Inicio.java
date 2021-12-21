a627fb47fb17d23a46462f197e66899dfe4321ac
camilo09m <c.nanco01@ufromail.cl>
2021-12-21 02:39:28 -0300

    Commit final 3

---
+++ src/main/java/Model/Inicio.java
@@ -0,0 +1,15 @@
+package Model;
+
+import GUIs.Login1;
+
+import java.util.Scanner;
+
+public class Inicio {
+    private String seleccion;
+
+    public Inicio(){
+        Login1 login1 = new Login1();
+        login1.setVisible(true);
+    }
+
+}
